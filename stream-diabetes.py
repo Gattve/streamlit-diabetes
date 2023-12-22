89deed5c1176e81882d49ab0f19f88d1d735a849
Gunawan <120243096+Gattve@users.noreply.github.com>
2023-12-22 09:31:09 +0700

    Add files via upload

---
+++ stream-diabetes.py
@@ -0,0 +1,46 @@
+import pickle
+import streamlit as st
+
+# Membaca model
+diabetes_model  = pickle.load(open('diabetes_model.sav', 'rb'))
+
+#  Judul web
+st.title('Data Mining Prediksi Diabetes')
+
+# Memabagi kolomnya
+col1, col2 = st.columns(2)
+
+with col1 :
+    Pregnancies = st.text_input ('Masukkan nilai Pregnancies')
+
+with col2 : 
+    Glucose = st.text_input ('Masukkan nilai Glucose')
+
+with col1 :
+    BloodPressure = st.text_input ('Masukkan nilai Blood Pressure')
+
+with col2 :
+    SkinThickness = st.text_input ('Masukkan nilai Skin Thickness')
+
+with col1 :
+    Insulin = st.text_input ('Masukkan nilai Insulin')
+
+with col2 :
+    BMI = st.text_input ('Masukkan nilai BMI')
+
+with col1 :
+    DiabetesPedigreeFunction = st.text_input ('Masukkan nilai Diabetes Pedigree Function')
+
+with col2 :
+    Age = st.text_input ('Masukkan nilai Age')
+
+# Membuat tombol untuk prediksi
+if st.button('Cek Predisi Diabetes'):
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    
+    if(diab_prediction[0] == 1):
+        diab_diagnosis = 'Pasien terkena Diabetes'
+    else : 
+        diab_diagnosis = 'Pasien tidak terkena Diabetes'
+        
+    st.success(diab_diagnosis)
